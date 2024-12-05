51351f69834706543c154ee2ae213ab0cb22b12f
Geraldine Fiser <geraldine@bluebear.tech>
2024-12-05 15:32:59 -0500

    [Classification] Generating text to classify github issues in specific categories
    Not all issues where categorized and the output format was left to the model.

--- app/(2-classification)/classification.ts
+++ app/(2-classification)/classification.ts
@@ -1,8 +1,17 @@
 import "dotenv/config";
 import supportRequests from "./support_requests.json";
+import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
 
 async function main() {
   console.log(supportRequests.slice(0, 2));
+  const result = await generateText({
+    model: openai("gpt-4o-mini"),
+    prompt:
+      "Classify the following support requests. The categories are (billing, product issues, enterprise sales, account issues, product feedback).\n\n" +
+      JSON.stringify(supportRequests),
+  });
+  console.log(result.text);
 }
 
 main().catch(console.error);
