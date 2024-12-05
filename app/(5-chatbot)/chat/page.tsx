e4ddd690145d716d04a7af98826f40bba457407c
Geraldine Fiser <geraldine@bluebear.tech>
2024-12-05 17:14:05 -0500

    [Chatbot] Setting up a chatbot with streamText
    * Passing the messages from the request to the streamText
    * Using the useChat() hook from ai/react which handles the input, change in input and submission

--- app/(5-chatbot)/chat/page.tsx
+++ app/(5-chatbot)/chat/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+import { useChat } from "ai/react";
+
 export default function Page() {
-  return <div>Chatbot</div>;
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map((message) => (
+        <div key={message.id} className="whitespace-pre-wrap">
+          {message.role === "user" ? "User: " : "AI: "}
+          {message.content}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Type your message"
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+        />
+      </form>
+    </div>
+  );
 }
