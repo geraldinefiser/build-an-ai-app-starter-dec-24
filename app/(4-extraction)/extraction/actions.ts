1dce5deb2034239df8225d88fbee449c90447c45
Geraldine Fiser <geraldine@bluebear.tech>
2024-12-05 16:48:36 -0500

    [Structured Extraction] Adding zod description to clarify the expected format for each output

--- app/(4-extraction)/extraction/actions.ts
+++ app/(4-extraction)/extraction/actions.ts
@@ -8,12 +8,24 @@ export const extractAppointment = async (input: string) => {
     model: openai("gpt-4o-mini"),
     prompt: `Extract appointment info from the input: ${input}`,
     schema: z.object({
-      title: z.string(),
-      startTime: z.string(),
-      endTime: z.string(),
-      attendees: z.array(z.string()),
-      location: z.string(),
-      date: z.string(),
+      title: z
+        .string()
+        .describe(
+          "The title of the event. This should be the main purpose of the event. No need to mention names. Clean up formatting (capitalise)."
+        ),
+      startTime: z.string().nullable().describe("format HH:MM"),
+      endTime: z
+        .string()
+        .nullable()
+        .describe("format HH:MM - note: default meeting duration is 1 hour"),
+      attendees: z
+        .array(z.string())
+        .nullable()
+        .describe("comma separated list of attendees"),
+      location: z.string().nullable(),
+      date: z
+        .string()
+        .describe("Today's date is: " + new Date().toISOString().split("T")[0]),
     }),
   });
   return result.object;
