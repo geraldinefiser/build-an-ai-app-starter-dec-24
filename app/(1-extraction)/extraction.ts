d0e525c4d4f3a181c9cc7bcfb0e9e6b9bd4f213f
Geraldine Fiser <geraldine@bluebear.tech>
2024-12-05 15:09:38 -0500

    [Extraction] from text file - get names mentioned in document
    * Uses pnpm --file name-- to run the function in terminal
    * Upgraded to Tier 1 paid API key on OpenAI to have access to gpt-4o-mini model

--- app/(1-extraction)/extraction.ts
+++ app/(1-extraction)/extraction.ts
@@ -1,11 +1,21 @@
 import "dotenv/config";
 import fs from "fs";
 
+// AI SDK Core module
+import { generateText } from "ai";
+
+// Model provider
+import { openai } from "@ai-sdk/openai";
+
 // import essay
 const essay = fs.readFileSync("app/(1-extraction)/essay.txt", "utf-8");
 
 async function main() {
-  console.log(essay.slice(0, 100) + "...");
+  const result = await generateText({
+    model: openai("gpt-4o-mini"),
+    prompt: "Extract all the names mentioned in this essay." + "\n\n" + essay,
+  });
+  console.log(result.text);
 }
 
 main().catch(console.error);
